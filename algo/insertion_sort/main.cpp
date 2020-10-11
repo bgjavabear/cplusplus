d1fff1acbb02adb958fce85ca051f6812f22966f
Alexander Kiselev <agvarelex@gmail.com>
2020-10-11 15:58:32 +0300

    refactoring insertion sort

--- algo/insertion_sort/main.cpp
+++ algo/insertion_sort/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "../../utils/utils.h"
 
-using namespace std;
+using namespace bgagvabear;
 
 void insertionSort(int *input, int size);
 
 int main() {
     int input[10] = {16, 2, 3, 75, 46, 0, 4, 123, 78, 11};
     int inputSize = *(&input + 1) - input;
-    bgagvabear::performance_check(insertionSort, input, inputSize);
+    performance_check(insertionSort, input, inputSize);
 }
 
 void insertionSort(int *input, int size) {
@@ -16,7 +16,7 @@ void insertionSort(int *input, int size) {
     while (i < size) {
         int j = i;
         while (j > 0 && input[j - 1] > input[j]) {
-            bgagvabear::swap(j, j - 1, input);
+            swap(j, j - 1, input);
             j--;
         }
         i++;
